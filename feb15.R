9afa83554610e299d1188df50218a25c80b640ff
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2026-02-15 14:25:22 +0600

    r files

---
+++ feb15.R
@@ -0,0 +1,53 @@
+
+
+
+#15 feb
+
+num_vec <- c(1, 2, 3, 4, 5)
+num_vec
+
+num_vec[3] <- 30
+
+num_vec[3] <- 35
+
+num_vec[c(2, 4)] <- c(25, 45)
+
+num_vec1 <- c("1", 2, 3, 4, 5)
+num_vec1
+
+
+num_vec2 <- c(1, 2, 3, 4, 5, "Apple")
+num_vec2
+
+
+char_vec <- c("Apple", "Banana", "Cherry")
+char_vec
+
+
+char_vec1 <- c(Apple, "Banana", "Cherry")
+char_vec1
+
+
+log_vec <- c(TRUE, FALSE, TRUE, FALSE)
+log_vec
+
+
+
+#arithmatic
+
+vec1 <- c(2, 4, 6)
+vec2 <- c(1, 3, 5)
+
+sum_vec <- vec1+vec2
+min_vec <- vec1-vec2
+
+vec3 <- c(4, 6, 8)
+vec4 <- c(2, 3, 4)
+
+div_vec <- vec3/vec4
+mul_vec <- vec3*vec4
+
+
+mod_vec <- vec1%%vec2
+
+power_vec <- vec1^2
